05f748255fed91f9f85750be13ba688cb16daa6d
Mike Gibson <mike@mgdigital.co.uk>
2014-06-08 15:47:25 +0100

    Cleaned things up

--- src/Application.php
+++ src/Application.php
@@ -4,10 +4,11 @@ namespace Layer;
 
 use Knp\Provider\ConsoleServiceProvider;
 use Layer\Action\ActionDispatcher;
+use Layer\Action\SimpleAction;
 use Layer\Asset\AssetServiceProvider;
 use Layer\Config\ConfigServiceProvider;
-use Layer\Config\Configuration;
 use Layer\Data\DataProvider;
+use Layer\Node\ControllerNode;
 use Layer\Node\ControllerNodeInterface;
 use Layer\Plugin\PluginServiceProvider;
 use Layer\Route\UrlMatcher;
@@ -24,13 +25,18 @@ use Silex\Application\UrlGeneratorTrait;
 use Silex\Provider\FormServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
 use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\RememberMeServiceProvider;
+use Silex\Provider\SecurityServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\SessionServiceProvider;
 use Silex\Provider\SwiftmailerServiceProvider;
 use Silex\Provider\TranslationServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
+use Symfony\Component\Debug\ErrorHandler;
+use Symfony\Component\Debug\ExceptionHandler;
 use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 /**
@@ -51,10 +57,14 @@ class Application extends \Silex\Application {
 
 		$app = $this;
 
-		$this->setPaths();
+		foreach($this->getPaths() as $key => $path) {
+			$app['paths.' . $key] = $path;
+		}
+
+		$this->registerServiceProviders();
 
 		$app['class_loader'] = $app->share(function() use($app) {
-			return require $app['path_vendor'] . '/autoload.php';
+			return require $app['paths.vendor'] . '/autoload.php';
 		});
 
 		/**
@@ -69,57 +79,6 @@ class Application extends \Silex\Application {
 		$app['string_helper'] = $app->share(function () use ($app) {
 			return new StringHelper($app);
 		});
-
-		$app->register(new ConfigServiceProvider());
-
-		/**
-		 * Load config files
-		 */
-		$app->extend('config', function(Configuration $config) use($app) {
-			$config->load($this->getConfigAutoload());
-			return $config;
-		});
-
-		$app['debug'] = $app->protect(function() use($app) {
-			return $app['config']->read('debug');
-		});
-
-		$this->registerErrorHandlers();
-
-		/**
-		 * Register service providers
-		 */
-		$app->register(new PluginServiceProvider());
-		$app->register(new ServiceControllerServiceProvider());
-		$app->register(new UrlGeneratorServiceProvider());
-		$app->register(new SessionServiceProvider(), [
-			'session.storage.save_path' => $app['path_session']
-		]);
-		$app->register(new HttpFragmentServiceProvider());
-		$app->register(new SwiftmailerServiceProvider());
-		$app->register(new ConsoleServiceProvider(), [
-			'console.name' => 'Layer Console',
-			'console.version' => '1.0.0',
-			'console.project_directory' => $app['path_root']
-		]);
-		$app->register(new TwigServiceProvider(), [
-			'twig.options' => [
-				'cache' => $app['path_cache'] . '/twig',
-				'auto_reload' => true
-			]
-		]);
-		//   $app['assetic.path_to_source'] = $app['path_resources'];
-		// $app['assetic.path_to_web'] = $app['path_public_assets'];
-		$app->register(new AssetServiceProvider());
-		$app->register(new TranslationServiceProvider(), [
-			'locale_fallbacks' => array('en'),
-		]);
-		$app->register(new FormServiceProvider());
-		// @todo set $app['form.secret'] to something better
-		$app->register(new MonologServiceProvider(), array(
-			'monolog.logfile' => $app['path_log'] . '/development.log',
-		));
-		$app->register(new DataProvider());
 
 		$app['route_class'] = 'Layer\\Route\\Route';
 
@@ -142,35 +101,74 @@ class Application extends \Silex\Application {
 			return $asset;
 		});
 
-		$app['nodes.matcher'] = $app->protect(function(ControllerNodeInterface $node, $key = 'node') {
-			return function(array $attrs) use($node, $key) {
-				try {
-					$nodePath = trim($attrs[$key], '/');
-					if($nodePath !== '') {
-						$node = $node->getDescendent($nodePath);;
+		$app['nodes.matcher'] = $app->protect(
+			function(ControllerNodeInterface $node, $key = 'node', $rejectNotFound = true) {
+				return function(array $attrs) use($node, $key, $rejectNotFound) {
+					try {
+						$nodePath = trim($attrs[$key], '/');
+						if($nodePath !== '') {
+							$node = $node->getDescendent($nodePath);;
+						}
+						$attrs[$key] = $node;
+					} catch(\InvalidArgumentException $e) {
+						if($rejectNotFound) {
+							return false;
+						}
 					}
-					$attrs[$key] = $node;
-				} catch(\InvalidArgumentException $e) {
-					return false;
-				}
-				return $attrs;
-			};
-		});
+					return $attrs;
+				};
+			}
+		);
 
 		$app['nodes.dispatcher'] = $app->protect(function($key = 'node') use($app) {
 			return function(Request $request) use($app, $key) {
-				return $app['actions.dispatcher']->dispatch($request->get($key), $request);
+				$node = $request->get($key);
+				if(!$node instanceof ControllerNodeInterface) {
+					$app->abort(404);
+				}
+				return $app['actions.dispatcher']->dispatch($node, $request);
 			};
 		});
 
-		$app['nodes.controllers_factory'] = $app->protect(function(ControllerNodeInterface $rootNode, $key = 'node') use($app) {
-			$controllers = $app['controllers_factory'];
-			$controllers->match('/{' . $key . '}', $app['nodes.dispatcher']($key))
-				->assert($key, '.*')
-				->beforeMatch($app['nodes.matcher']($rootNode, $key))
-				->bind($rootNode->getRouteName());
-			return $controllers;
+		$app['nodes.controllers_factory'] = $app->protect(
+			function(ControllerNodeInterface $rootNode, $key = 'node', $rejectNotFound = true) use($app) {
+				$controllers = $app['controllers_factory'];
+				$controllers->match('/{' . $key . '}', $app['nodes.dispatcher']($key))
+					->assert($key, '.*')
+					->beforeMatch($app['nodes.matcher']($rootNode, $key, $rejectNotFound))
+					->bind($rootNode->getRouteName());
+				return $controllers;
+			}
+		);
+
+		$app['app.home_template'] = 'view/home';
+
+		$app['app.home_action'] = $app->share(function() use($app) {
+			return new SimpleAction('home', 'Home', $app['app.home_template']);
 		});
+
+		$app['app.home_node'] = $app->share(function() use($app) {
+			return new ControllerNode('app', $app['app.home_action']);
+		});
+
+		$app['app.controllers'] = $app->share(function() use($app) {
+			return $app['nodes.controllers_factory']($app['app.home_node']);
+		});
+
+		$app['debug'] = $app->protect(function() use($app) {
+			return $app['config']->read('debug');
+		});
+
+		$app['config.autoload'] = [
+			'app' => [
+				'nest' => false
+			],
+			'database',
+			'local' => [
+				'nest' => false,
+				'ignoreMissing' => true
+			]
+		];
 
 	}
 
@@ -181,12 +179,9 @@ class Application extends \Silex\Application {
 
 		if (!$this->booted) {
 
-			/**
-			 * Set the timezone
-			 */
-			if ($timezone = $this->config('timezone')) {
-				date_default_timezone_set($timezone);
-			}
+			$this->registerErrorHandlers();
+			$this->setTimezone();
+			$this->initializeSecurity();
 
 			$this['assetic.asset_manager']->set('js_modernizr', $this['assets.js.modernizr']);
 
@@ -240,35 +235,24 @@ class Application extends \Silex\Application {
 		return parent::handle($request, $type, $catch);
 	}
 
-	protected function setPaths() {
-		$this['path_root'] = realpath(__DIR__ . '/../../../..');
-		$this['path_app'] = $this['path_root'] . '/app';
-		$this['path_config'] = $this['path_app'] . '/Config';
-		$this['path_resources'] = $this['path_app'] . '/Resource';
-		$this['path_templates'] = $this['path_app'] . '/Template';
-		$this['path_vendor'] = $this['path_root'] . '/vendor';
-		$this['path_public'] = $this['path_root'] . '/public';
-		//   $this['path_public_assets'] = $this['path_public'] . '/assets';
-		$this['path_storage'] = $this['path_root'] . '/storage';
-		$this['path_log'] = $this['path_storage'] . '/log';
-		$this['path_tmp'] = $this['path_storage'] . '/tmp';
-		$this['path_cache'] = $this['path_tmp'] . '/cache';
-		$this['path_session'] = $this['path_tmp'] . '/session';
-		$this['path_layer'] = __DIR__;
-		$this['path_layer_resources'] = $this['path_layer'] . '/Resource';
-	}
+	protected function getPaths() {
 
-	protected function getConfigAutoload() {
-		return [
-			'app' => [
-				'nest' => false
-			],
-			'database',
-			'local' => [
-				'nest' => false,
-				'ignoreMissing' => true
-			]
-		];
+		$root = realpath(__DIR__ . '/../../../..');
+		$app = $root . '/app';
+		$config = $app . '/Config';
+		$resources = $app . '/Resource';
+		$templates = $app . '/Template';
+		$vendor = $root . '/vendor';
+		$public = $root . '/public';
+		//   $this['paths.public_assets'] = $this['paths.public'] . '/assets';
+		$storage = $root . '/storage';
+		$log = $storage . '/log';
+		$tmp = $storage . '/tmp';
+		$cache = $tmp . '/cache';
+		$session = $tmp . '/session';
+		$layer = __DIR__;
+		return compact('root', 'app', 'config', 'resources', 'templates', 'vendor',
+			'public', 'storage', 'log', 'tmp', 'cache', 'session', 'layer');
 	}
 
 	protected function registerErrorHandlers() {
@@ -280,23 +264,72 @@ class Application extends \Silex\Application {
 
 			error_reporting(-1);
 
-			\Symfony\Component\Debug\ErrorHandler::register();
+			ErrorHandler::register();
 
 			if ('cli' !== php_sapi_name()) {
-				\Symfony\Component\Debug\ExceptionHandler::register();
+				ExceptionHandler::register();
 				// CLI - display errors only if they're not already logged to STDERR
 			} elseif (!ini_get('log_errors') || ini_get('error_log')) {
 				ini_set('display_errors', 1);
 			}
 
 			$this->error(function (\Exception $e) {
-				return new \Symfony\Component\HttpFoundation\Response(
+				return new Response(
 					nl2br($e->getMessage() . PHP_EOL . $e->getTraceAsString())
 				);
 			});
 
 		}
 
+	}
+
+	protected function registerServiceProviders() {
+		$this->register(new ConfigServiceProvider());
+		$this->register(new PluginServiceProvider());
+		$this->register(new ServiceControllerServiceProvider());
+		$this->register(new UrlGeneratorServiceProvider());
+		$this->register(new SessionServiceProvider(), [
+			'session.storage.save_path' => $this['paths.session']
+		]);
+
+		$this->register(new HttpFragmentServiceProvider());
+		$this->register(new SwiftmailerServiceProvider());
+		$this->register(new ConsoleServiceProvider(), [
+			'console.name' => 'Layer Console',
+			'console.version' => '1.0.0',
+			'console.project_directory' => $this['paths.root']
+		]);
+		$this->register(new TwigServiceProvider(), [
+			'twig.options' => [
+				'cache' => $this['paths.cache'] . '/twig',
+				'auto_reload' => true
+			]
+		]);
+		$this->register(new AssetServiceProvider());
+		$this->register(new TranslationServiceProvider(), [
+			'locale_fallbacks' => ['en'],
+		]);
+		$this->register(new FormServiceProvider());
+		// @todo set $this->app['form.secret'] to something better
+		$this->register(new MonologServiceProvider(), [
+			'monolog.logfile' => $this['paths.log'] . '/development.log',
+		]);
+		$this->register(new DataProvider());
+	}
+
+	protected function setTimezone() {
+		if ($timezone = $this['config']->read('timezone')) {
+			date_default_timezone_set($timezone);
+		} else {
+			throw new \RuntimeException('No timezone has been configured.');
+		}
+	}
+
+	protected function initializeSecurity() {
+		if(!empty($this['security.firewalls'])) {
+			$this->register(new SecurityServiceProvider());
+			$this->register(new RememberMeServiceProvider());
+		}
 	}
 
 }
