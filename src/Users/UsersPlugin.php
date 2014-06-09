59697b5dcf84b8deeb6d89e9cd0b30bc49b50a33
Mike Gibson <mike@mgdigital.co.uk>
2014-06-09 19:07:42 +0100

    Removed unneeded stuff

--- src/Users/UsersPlugin.php
+++ src/Users/UsersPlugin.php
@@ -3,11 +3,7 @@
 namespace Layer\Users;
 
 use Doctrine\ORM\EntityManagerInterface;
-use Layer\Node\ControllerNode;
-use Layer\Node\ControllerNodeInterface;
 use Layer\Plugin\Plugin;
-use Layer\Users\Action\LoginAction;
-use Layer\Users\Form\LoginFormType;
 
 class UsersPlugin extends Plugin {
 
@@ -31,37 +27,11 @@ class UsersPlugin extends Plugin {
 			return new UserListener($this->app['security.encoder_factory']);
 		});
 
-		$this->app['users.login_form_type'] = $this->app->share(function() {
-			return new LoginFormType();
-		});
-
-		$this->app['users.login_action'] = $this->app->share(function() {
-			return new LoginAction($this->app['form.factory'], $this->app['users.login_form_type']);
-		});
-
-		$this->app['users.login_node'] = $this->app->share(function() {
-			return new ControllerNode('login', $this->app['users.login_action']);
-		});
-
 		$this->app->extend('orm.em', function(EntityManagerInterface $entityManager) {
 			$entityManager->getEventManager()->addEventSubscriber($this->app['users.listener']);
 			return $entityManager;
 		});
 
-		$this->app->extend('app.home_node', function(ControllerNodeInterface $node) {
-			$node->wrapChildNode($this->app['users.login_node']);
-			return $node;
-		});
-
-	/*	$firewalls = $this->app['security.firewalls'];
-		$firewalls['form'] = [
-			'form' => [
-				'login_path' => '/login',
-				'check_path' => '/login'
-			]
-		];
-		$this->app['security.firewalls'] = $firewalls;
-*/
 	}
 
 	public function boot() {
