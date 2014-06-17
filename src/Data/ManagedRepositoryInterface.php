e95382da07faeeb72a855981a2f97b49aea105f6
Mike Gibson <mike@mgdigital.co.uk>
2014-06-17 21:45:39 +0100

    Added comments

--- src/Data/ManagedRepositoryInterface.php
+++ src/Data/ManagedRepositoryInterface.php
@@ -6,32 +6,82 @@ use Doctrine\Common\Persistence\ObjectRepository;
 
 interface ManagedRepositoryInterface extends ObjectRepository {
 
+	/**
+	 * @param $name
+	 * @param array $options
+	 * @return mixed
+	 */
 	public function queryMetadata($name, array $options = []);
 
+	/**
+	 * @return \Doctrine\ORM\Mapping\ClassMetadata
+	 */
 	public function getClassMetadata();
 
+	/**
+	 * @return string
+	 */
 	public function getName();
 
+	/**
+	 * @return object
+	 */
 	public function createEntity();
 
+	/**
+	 * @param null $alias
+	 * @param null $indexBy
+	 * @return \Doctrine\ORM\QueryBuilder
+	 */
 	public function createQueryBuilder($alias = null, $indexBy = null);
 
+	/**
+	 * @param $alias
+	 * @return \Doctrine\ORM\ResultSetMappingBuilder
+	 */
 	public function createResultSetMappingBuilder($alias);
 
+	/**
+	 * @param $queryName
+	 * @return \Doctrine\ORM\Query
+	 */
 	public function createNamedQuery($queryName);
 
+	/**
+	 * @param $queryName
+	 * @return \Doctrine\ORM\NativeQuery
+	 */
 	public function createNativeNamedQuery($queryName);
-
-	//public function createFormType(MetadataDriverInterface $driver = null);
 
 	public function clear();
 
+	/**
+	 * @param mixed $id
+	 * @param null $lockMode
+	 * @param null $lockVersion
+	 * @return object
+	 */
 	public function find($id, $lockMode = null, $lockVersion = null);
 
+	/**
+	 * @return array
+	 */
 	public function findAll();
 
+	/**
+	 * @param array $criteria
+	 * @param array $orderBy
+	 * @param null $limit
+	 * @param null $offset
+	 * @return array
+	 */
 	public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null);
 
+	/**
+	 * @param array $criteria
+	 * @param array $orderBy
+	 * @return object
+	 */
 	public function findOneBy(array $criteria, array $orderBy = null);
 
 	public function save($entity);
