c9b5b2c069cb18977ba47954967860f01c1d5548
Mike Gibson <mike@mgdigital.co.uk>
2014-06-17 21:40:52 +0100

    Added comments

--- src/Cms/Data/CmsRepositoryInterface.php
+++ src/Cms/Data/CmsRepositoryInterface.php
@@ -7,14 +7,33 @@ use Layer\Node\ControllerNodeInterface;
 
 interface CmsRepositoryInterface extends ManagedRepositoryInterface {
 
+	/**
+	 * @return string
+	 */
 	public function getCmsSlug();
 
+	/**
+	 * @return CmsRepositoryInterface
+	 */
 	public function getRootCmsNode();
 
+	/**
+	 * @param $name
+	 * @return bool
+	 */
 	public function hasCmsNode($name);
 
+	/**
+	 * @param $name
+	 * @return CmsRepositoryInterface
+	 * @throws \InvalidArgumentException
+	 */
 	public function getCmsNode($name);
 
+	/**
+	 * @param ControllerNodeInterface $node
+	 * @param bool $isRootNode
+	 */
 	public function registerCmsNode(ControllerNodeInterface $node, $isRootNode = false);
 
 }
