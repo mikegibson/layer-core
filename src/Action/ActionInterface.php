501d0bb5ce27ac09532f44be1da3308b7201a550
Mike Gibson <mike@mgdigital.co.uk>
2014-06-20 22:30:25 +0100

    Added comments

--- src/Action/ActionInterface.php
+++ src/Action/ActionInterface.php
@@ -6,14 +6,30 @@ use Symfony\Component\HttpFoundation\Request;
 
 interface ActionInterface {
 
+	/**
+	 * @return string
+	 */
 	public function getName();
 
+	/**
+	 * @return string
+	 */
 	public function getLabel();
 
+	/**
+	 * @return string
+	 */
 	public function getTemplate();
 
+	/**
+	 * @param Request $request
+	 * @return null|array|\Symfony\Component\HttpFoundation\Response
+	 */
 	public function invoke(Request $request);
 
+	/**
+	 * @return bool
+	 */
 	public function isVisible();
 
 }
