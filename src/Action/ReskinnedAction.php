b57f45d7c21ea052e1c6f493ef4190afb23a46c2
Mike Gibson <mike@mgdigital.co.uk>
2014-06-20 22:30:43 +0100

    Created ReskinnedAction

---
+++ src/Action/ReskinnedAction.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Layer\Action;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class ReskinnedAction implements ActionInterface {
+
+	/**
+	 * @var ActionInterface
+	 */
+	private $baseAction;
+
+	/**
+	 * @var string
+	 */
+	private $template;
+
+	/**
+	 * @param ActionInterface $baseAction
+	 * @param $template
+	 */
+	public function __construct(ActionInterface $baseAction, $template) {
+		$this->baseAction = $baseAction;
+		$this->template = $template;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName() {
+		return $this->getBaseAction()->getName();
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLabel() {
+		return $this->getBaseAction()->getLabel();
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTemplate() {
+		return $this->template;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isVisible() {
+		return $this->getBaseAction()->isVisible();
+	}
+
+	/**
+	 * @param Request $request
+	 * @return array|null|\Symfony\Component\HttpFoundation\Response
+	 */
+	public function invoke(Request $request) {
+		return $this->getBaseAction()->invoke($request);
+	}
+
+	/**
+	 * @return ActionInterface
+	 */
+	protected function getBaseAction() {
+		return $this->baseAction;
+	}
+
+}
