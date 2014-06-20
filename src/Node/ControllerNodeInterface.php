7244d06b3a35e1abe6466809ac0e8b456ffbb9e3
Mike Gibson <mike@mgdigital.co.uk>
2014-06-20 22:51:24 +0100

    added comments

--- src/Node/ControllerNodeInterface.php
+++ src/Node/ControllerNodeInterface.php
@@ -6,20 +6,45 @@ use Symfony\Component\HttpFoundation\Request;
 
 interface ControllerNodeInterface extends NodeInterface {
 
+	/**
+	 * @return string
+	 */
 	public function getRouteName();
 
+	/**
+	 * @return string
+	 */
 	public function getActionName();
 
+	/**
+	 * @return string
+	 */
 	public function getActionLabel();
 
+	/**
+	 * @param Request $request
+	 * @return array|null|\Symfony\Component\HttpFoundation\Response
+	 */
 	public function invoke(Request $request);
 
+	/**
+	 * @return string
+	 */
 	public function getTemplate();
 
+	/**
+	 * @return bool
+	 */
 	public function isAccessible();
 
+	/**
+	 * @return bool
+	 */
 	public function isVisible();
 
+	/**
+	 * @return array
+	 */
 	public function getVisibleChildNodes();
 
 }
