228de4b1f3fda494ad15e2b4b792ea985dcf4ce9
vagrant <vagrant@vagrant.vm>
2014-06-22 16:51:35 +0000

    Added datetime type extension

--- src/Form/FormServiceProvider.php
+++ src/Form/FormServiceProvider.php
@@ -10,17 +10,14 @@ class FormServiceProvider extends \Silex\Provider\FormServiceProvider {
 
 		parent::register($app);
 
-		$app['form.extensions.html'] = $app->share(function() use($app) {
-			return new HtmlExtension();
-		});
-
 		$app['form.extensions'] = $app->share($app->extend('form.extensions', function(array $extensions) use($app) {
-			$extensions[] = $app['form.extensions.html'];
+			$extensions[] = new HtmlExtension();
 			return $extensions;
 		}));
 
 		$app['form.type.extensions'] = $app->share($app->extend('form.type.extensions', function(array $extensions) use($app) {
 			$extensions[] = new FormTypeLayerExtension();
+			$extensions[] = new DateTimeTypeExtension();
 			return $extensions;
 		}));
 
