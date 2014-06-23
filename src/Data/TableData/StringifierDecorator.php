5bb041b5f765436039a5a847539c0f345e0f7bd2
Mike Gibson <mike@mgdigital.co.uk>
2014-06-23 21:41:44 +0100

    Improved stringifier

--- src/Data/TableData/StringifierDecorator.php
+++ src/Data/TableData/StringifierDecorator.php
@@ -15,19 +15,35 @@ class StringifierDecorator implements TableDataDecoratorInterface {
 	 * @return string
 	 */
 	public function decorateData($value, $key, $object) {
+		return $this->stringify($value);
+	}
+
+	protected function stringify($value, $force = true) {
 		if(is_array($value)) {
-			$value = '[ARRAY]';
+			return $this->stringifyArray($value);
 		} elseif(is_object($value) && !method_exists($value, '__toString')) {
 			if($value instanceof \DateTime) {
-				$value = $this->formatDateTime($value);
+				return $this->formatDateTime($value);
 			} else {
-				$value = sprintf('[OBJECT:%s]', get_class($value));
+				return $force ? sprintf('[OBJECT:%s]', get_class($value)) : false;
 			}
 		}
 		if(!settype($value, 'string')) {
-			return '';
+			return $force ? '' : false;
 		}
 		return $value;
+	}
+
+	protected function stringifyArray(array $array, $force = true) {
+		$parts = [];
+		foreach($array as $part) {
+			$part = $this->stringify($part, false);
+			if(!is_string($part)) {
+				return $force ? '[ARRAY]' : false;
+			}
+			$parts[] = $part;
+		}
+		return implode(', ', $parts);
 	}
 
 	protected function formatDateTime(\DateTime $dateTime) {
