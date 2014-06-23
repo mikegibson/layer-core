577f31f78d221db6a09e41471f66c36015f1a51f
Mike Gibson <mike@mgdigital.co.uk>
2014-06-23 20:26:33 +0100

    Added comments

--- src/Node/NodeInterface.php
+++ src/Node/NodeInterface.php
@@ -6,26 +6,68 @@ interface NodeInterface {
 
 	const SEPARATOR = '/';
 
+	/**
+	 * @return string
+	 */
 	public function getName();
 
+	/**
+	 * @return string
+	 */
 	public function getLabel();
 
+	/**
+	 * @return null|NodeInterface
+	 */
 	public function getParentNode();
 
+	/**
+	 * @return NodeInterface[]
+	 */
 	public function getChildNodes();
 
+	/**
+	 * @return string
+	 */
 	public function getPath();
 
+	/**
+	 * @param $path
+	 * @return NodeInterface
+	 */
 	public function getDescendent($path);
 
+	/**
+	 * @param $name
+	 * @return bool
+	 */
 	public function hasChildNode($name);
 
+	/**
+	 * @param $name
+	 * @return NodeInterface
+	 */
 	public function getChildNode($name);
 
+	/**
+	 * @return NodeInterface
+	 */
 	public function getRootNode();
 
+	/**
+	 * @param NodeInterface $childNode
+	 */
 	public function registerChildNode(NodeInterface $childNode);
 
+	/**
+	 * @param NodeInterface $baseNode
+	 * @param null $name
+	 * @param null $label
+	 * @param bool $baseChildrenAccessible
+	 * @param bool $overwrite
+	 * @param bool $prepend
+	 * @return NodeInterface
+	 */
 	public function wrapChildNode(
 		NodeInterface $baseNode,
 		$name = null,
@@ -35,8 +77,14 @@ interface NodeInterface {
 		$prepend = false
 	);
 
+	/**
+	 * @param NodeInterface $node
+	 */
 	public function adoptChildNodes(NodeInterface $node);
 
+	/**
+	 * @param callable $callback
+	 */
 	public function sortChildNodes($callback);
 
 }
