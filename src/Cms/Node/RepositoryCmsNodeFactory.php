faf75c9f50887b3dbc297630323faf6c99e94e24
Mike Gibson <mike@mgdigital.co.uk>
2014-06-29 18:37:41 +0100

    Check for existence of custom node path

--- src/Cms/Node/RepositoryCmsNodeFactory.php
+++ src/Cms/Node/RepositoryCmsNodeFactory.php
@@ -7,6 +7,7 @@ use Layer\Cms\Action\RepositoryCmsActionFactoryInterface;
 use Layer\Cms\Data\CmsRepositoryInterface;
 use Layer\Node\ControllerNode;
 use Layer\Node\ControllerNodeInterface;
+use Layer\Node\NodeInterface;
 
 class RepositoryCmsNodeFactory implements RepositoryCmsNodeFactoryInterface {
 
@@ -53,12 +54,23 @@ class RepositoryCmsNodeFactory implements RepositoryCmsNodeFactoryInterface {
 		}
 
 		if($actions) {
+			$parentNode = $this->rootCmsNode;
 			$name = $repository->getCmsSlug();
 			$label = $repository->queryMetadata('getEntityHumanName', ['plural' => true, 'capitalize' => true]);
 			$rootAction = array_shift($actions);
 			$repositoryBaseNode = $this->createNodeFromAction($rootAction);
-			$nodePath = $repository->queryMetadata('getCmsNodePath');
-			$parentNode = $nodePath ? $this->rootCmsNode->getDescendent($nodePath) : $this->rootCmsNode;
+			if($nodePath = $repository->queryMetadata('getCmsNodePath')) {
+				$parts = explode(NodeInterface::SEPARATOR, $nodePath);
+				$name = array_pop($parts);
+				if($parts) {
+					$nodePath = implode(NodeInterface::SEPARATOR, $parts);
+					try {
+						$parentNode = $parentNode->getDescendent($nodePath);
+					} catch(\InvalidArgumentException $e) {
+						throw new \RuntimeException(sprintf('CMS node path %s is not valid.', $nodePath));
+					}
+				}
+			}
 			$nodes[] = $repositoryRoot = $parentNode->wrapChildNode($repositoryBaseNode, $name, $label);
 			foreach($actions as $action) {
 				$nodes[] = $repositoryRoot->wrapChildNode($this->createNodeFromAction($action), $action->getName());
